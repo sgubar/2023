2442303a5cc2ce9993a2491d50159bc5ccf3cea2
Max Hayduk <73790260+haydukmax@users.noreply.github.com>
2023-05-19 12:48:22 +0300

    Update tree.h

--- DK21/Hayduk Max/LABA3/tree.h
+++ DK21/Hayduk Max/LABA3/tree.h
@@ -1,1 +1,17 @@
+#ifndef TREE_H
+#define TREE_H
 
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data);
+struct Node* insert(struct Node* root, int data);
+void printTree(struct Node* root);
+struct Node* search(struct Node* root, int data);
+struct Node* deleteNode(struct Node* root, int data);
+void deleteTree(struct Node* root);
+
+#endif
