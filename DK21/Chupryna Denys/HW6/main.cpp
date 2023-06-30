e8ccae8f65e7c4de3ed669ca7fbbf83090d6a1c0
Denys Chupryna <124678154+dedeneded@users.noreply.github.com>
2023-06-30 12:27:46 +0300

    Add files via upload

---
+++ DK21/Chupryna Denys/HW6/main.cpp
@@ -0,0 +1,68 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+float a = 0.8f;
+// Функція, що викликатиметься для малювання сцени
+void display()
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // Фон чорний та прозорий
+    glClear(GL_COLOR_BUFFER_BIT); // Очищення буферу кольору
+
+    // Малювання серії відрізків, що є графіком функції
+    glBegin(GL_LINE_STRIP); // Кожна пара вершини задає відрізок
+        glColor3f(1.0f, 0.0f, 0.0f);
+        for (float t= -M_PI; t <= M_PI; t += 0.1) {
+            float x = a*pow(sin(t), 3) ;
+            float y = a*pow(cos(t), 3) ;
+            glVertex2f(x , y);
+        }
+    glEnd();
+
+    // Малювання координатних осей
+    glBegin(GL_LINE_STRIP);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(0.0, -1.0);
+        glVertex2f(0.0, 1.0);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(-1.0, 0.0);
+        glVertex2f(1.0, 0.0);
+    glEnd();
+
+    // Малювання позначок на координатних осях
+    for( float x = -1.0; x <= 1.0; x += 0.1){
+        glBegin(GL_LINE_STRIP);
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glVertex2f(x, -0.05);
+            glVertex2f(x, 0.05);
+        glEnd();
+    }
+    for( float y = -1.0; y <= 1.0; y += 0.1){
+        glBegin(GL_LINE_STRIP);
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glVertex2f(-0.05, y);
+            glVertex2f(0.05, y);
+        glEnd();
+    }
+    glFlush(); // Виконати малювання
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv); // Ініціалізація GLUT
+    glutInitWindowSize(500, 500); // Ширина та висота вікна
+    glutInitWindowPosition(0, 0); // Положення вікна
+    glutCreateWindow("HW6"); // Створення вікна із заданим ім'ям
+    glutDisplayFunc(display); // Реєстрація функції зворотного
+    // виклику для малювання вікна
+    glutMainLoop(); // Вхід у цикл обробки подій GLUT
+return 0;
+}
